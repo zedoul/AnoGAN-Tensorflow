21281aaca2459968fcaa57ddfc7f592dc11ce5c4
lzb <lizhengbang@hust.edu.cn>
2017-12-11 14:32:39 -0500

    modify loss

--- model.py
+++ model.py
@@ -136,8 +136,12 @@ class DCGAN(object):
 
         # detect anomaly
         numda = 0.1
-        self.discriminator_loss = self.d_loss
-        self.residual_loss = 2 * tf.nn.l2_loss(self.inputs - self.G)
+        self.discriminator_loss = tf.reduce_mean(
+            sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)), [1, 2, 3]) + tf.reduce_mean(
+            sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)), [1, 2, 3])
+        print (self.discriminator_loss.shape)
+        self.residual_loss = tf.reduce_sum(tf.square(self.inputs - self.G), [1, 2, 3])
+        print (self.residual_loss.shape)
         self.complete_loss = (1 - numda) * self.residual_loss + numda * self.discriminator_loss
         self.grad_complete_loss = tf.gradients(self.complete_loss, self.z)
 
@@ -310,7 +314,7 @@ class DCGAN(object):
                 zhats = np.clip(zhats, -1, 1)
 
             for index in xrange(batchSz):
-                anomaly_score[l+index] = loss
+                anomaly_score[l+index] = loss[index]
             print ("num : {}  cost time : {}".format(idx, time.time() - start))
 
         # TODO : deal with anomaly_score(loss vector)
