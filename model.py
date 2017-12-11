3c1194e7a1804fb6dfe286b9b32cf4329f21d652
lzb <lizhengbang@hust.edu.cn>
2017-12-11 11:31:22 -0500

    add evaluation

--- model.py
+++ model.py
@@ -136,8 +136,11 @@ class DCGAN(object):
         # detect anomaly
         numda = 0.1
         self.discriminator_loss = self.d_loss
+        print (self.discriminator_loss.shape)
         self.residual_loss = 2 * tf.nn.l2_loss(self.inputs - self.G)
+        print (self.residual_loss.shape)
         self.complete_loss = (1 - numda) * self.residual_loss + numda * self.discriminator_loss
+        print (self.complete_loss.shape)
         self.grad_complete_loss = tf.gradients(self.complete_loss, self.z)
 
     def train(self, config):
@@ -299,15 +302,15 @@ class DCGAN(object):
             m = 0
             v = 0
 
-            # nRows = np.ceil(batchSz / 8)
-            # nCols = min(8, batchSz)
-            # save_images(batch_images[:batchSz, :, :, :], [nRows, nCols],
-            #             os.path.join(config.outDir, 'before.png'))
-            # for img in range(batchSz):
-            #     with open(os.path.join(config.outDir, 'logs/hats_{:02d}.log'.format(img)), 'a') as f:
-            #         f.write('iter loss ' +
-            #                 ' '.join(['z{}'.format(zi) for zi in range(self.z_dim)]) +
-            #                 '\n')
+            nRows = np.ceil(batchSz / 8)
+            nCols = min(8, batchSz)
+            save_images(batch_images[:batchSz, :, :, :], [nRows, nCols],
+                        os.path.join(config.outDir, 'before.png'))
+            for img in range(batchSz):
+                with open(os.path.join(config.outDir, 'logs/hats_{:02d}.log'.format(img)), 'a') as f:
+                    f.write('iter loss ' +
+                            ' '.join(['z{}'.format(zi) for zi in range(self.z_dim)]) +
+                            '\n')
 
             loss = 0
             G_imgs = 0
